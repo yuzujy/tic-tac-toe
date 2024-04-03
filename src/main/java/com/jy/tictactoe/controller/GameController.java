11112b06aec058d4a2e0d0741393d8218bd0348c
yuzujy <e0968312@u.nus.edu>
2024-04-03 19:07:12 +0800

    updated session feature in backend

--- src/main/java/com/jy/tictactoe/controller/GameController.java
+++ src/main/java/com/jy/tictactoe/controller/GameController.java
@@ -13,17 +13,20 @@ import com.jy.tictactoe.storage.GameStorage;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.servlet.http.HttpSession;
+import jakarta.servlet.http.HttpSession;
 
 @RestController
 @Slf4j
@@ -35,15 +38,19 @@ public class GameController {
     private final SimpMessagingTemplate simpMessagingTemplate;
 
     @PostMapping("/start")
-    public ResponseEntity<Game> start(@RequestBody Player player) {
+    public ResponseEntity<Game> start(@RequestBody Player player, HttpSession session) {
         log.info("start game request: {}", player);
-        return ResponseEntity.ok(gameService.createGame(player));
+        Game game = gameService.createGame(player);
+        updateGameHistoryInSession(game.getGameId(), session);
+        return ResponseEntity.ok(game);
     }
 
     @PostMapping("/connect")
-    public ResponseEntity<Game> connect(@RequestBody ConnectRequest request) throws InvalidParamException, InvalidGameException {
+    public ResponseEntity<Game> connect(@RequestBody ConnectRequest request, HttpSession session) throws InvalidParamException, InvalidGameException {
         log.info("connect request: {}", request);
-        return ResponseEntity.ok(gameService.connectToGame(request.getPlayer(), request.getGameId()));
+        Game game = gameService.connectToGame(request.getPlayer(), request.getGameId());
+        updateGameHistoryInSession(game.getGameId(), session);
+        return ResponseEntity.ok(game);
     }
 
     @PostMapping("/connect/random")
@@ -53,21 +60,50 @@ public class GameController {
     }
 
     @PostMapping("/gameplay")
-    public ResponseEntity<Game> gamePlay(@RequestBody GamePlay request) throws NotFoundException, InvalidGameException {
+    public ResponseEntity<Game> gamePlay(@RequestBody GamePlay request, HttpSession session) throws NotFoundException, InvalidGameException {
         log.info("gameplay: {}", request);
         Game game = gameService.gamePlay(request);
         simpMessagingTemplate.convertAndSend("/topic/game-progress/" + game.getGameId(), game);
         return ResponseEntity.ok(game);
     }
 
-    @PostMapping("/history/{gameId}")
-    public ResponseEntity<List<String>> getGameHistory(@PathVariable String gameId) throws NotFoundException {
-        Game game = GameStorage.getInstance().getGames().get(gameId);
-        if (game == null) {
-            throw new NotFoundException("Game not found");
+    // @PostMapping("/history/{gameId}")
+    // public ResponseEntity<List<String>> getGameHistory(@PathVariable String gameId, HttpSession session) throws NotFoundException {
+    //     Game game = GameStorage.getInstance().getGames().get(gameId);
+    //     if (game == null) {
+    //         throw new NotFoundException("Game not found");
+    //     }
+    //     return ResponseEntity.ok(game.getMoveHistoryDescriptions());
+    // }
+
+    @GetMapping("/history")
+    public ResponseEntity<List<Game>> getGameHistory(HttpSession session) {
+        List<String> gameIds = (List<String>) session.getAttribute("gameHistory");
+        if (gameIds == null) {
+            gameIds = new ArrayList<>();
         }
-        return ResponseEntity.ok(game.getMoveHistoryDescriptions());
+        List<Game> games = gameIds.stream()
+                                  .map(gameId -> GameStorage.getInstance().getGames().get(gameId))
+                                  .collect(Collectors.toList());
+        return ResponseEntity.ok(games);
     }
+
+
+    private void updateGameHistoryInSession(String gameId, HttpSession session) {
+        List<String> gameHistory = (List<String>) session.getAttribute("gameHistory");
+        if (gameHistory == null) {
+            gameHistory = new ArrayList<>();
+        }
+        gameHistory.add(gameId);
+        session.setAttribute("gameHistory", gameHistory);
+    }
+
+    @PostMapping("/newSession")
+    public ResponseEntity<?> startNewSession(HttpSession session) {
+        session.removeAttribute("gameHistory"); // Clear the game history from the session
+        return ResponseEntity.ok().build();
+    }
+
 
     
 }
